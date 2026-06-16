46b3163779bea2f91ab2c901a9f1cdb24076a96a
mandy-tsoi-ISS <mandy.tsoi@hk.issworld.com>
2026-06-16 05:04:07 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+      <h1>Two Truths and a Lie 🎉</h1>
+      <p>If you see this, the deployment is working correctly!</p>
+    </div>
+  );
+}
+
+export default App;
