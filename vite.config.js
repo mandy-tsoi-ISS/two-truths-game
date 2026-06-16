f5a91cf836bfc5593fc63f2bd3d535f697ee9d5f
mandy-tsoi-ISS <mandy.tsoi@hk.issworld.com>
2026-06-16 05:31:10 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Replace with your actual GitHub repository name
+  // Make sure 'two-truths-game' matches your repository name exactly
   base: '/two-truths-game/', 
 })
